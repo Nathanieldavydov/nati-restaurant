131fac3d0bda285640aa4b46ef84f6f4cd1f3cfa
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-11-17 13:33:45 -0500

    update

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          
+          <p className="p__opensans">To create great food, you must do it from the heart.</p>
+        </div>
+        <p className="p__opensans"> To eat good food, it gives warmth to the heart.</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Abigail Davydov</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
